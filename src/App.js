ee0f420088efdb00214767485ab08567923685de
Roger Chapman <roger.chapman@safetyculture.io>
2019-04-10 16:47:03 +1000

    Simple list of the gRPC requests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+/*global chrome*/
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    requests: [],
+  }
+
+  componentDidMount() {
+    if (chrome && chrome.devtools) {
+      chrome.devtools.network.onRequestFinished.addListener(rtn => {
+        if (rtn.request.postData.mimeType == "application/grpc-web-text") {
+          const { requests } = this.state;
+          requests.push(rtn.request.url);
+          this.setState({ requests });
+        }
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.requests.map(element => {
+          return <div>{element}</div>
+        })}
       </div>
     );
   }
