f7889651585bb55ad904e1a2e8a3808779f8002f
Roger Chapman <roger.chapman@safetyculture.io>
2019-04-17 11:19:56 +1000

    Enable scolling in the details view

--- src/components/NetworkDetails.js
+++ src/components/NetworkDetails.js
@@ -3,12 +3,13 @@
 import React, { Component } from 'react';
 import ReactJson from 'react-json-view';
 import { connect } from 'react-redux';
+import './NetworkDetails.css';
 
 class NetworkDetails extends Component {
   render() {
     const { entry } = this.props;
     return (
-      <div className="widget vbox">
+      <div className="widget vbox details-data">
         {this._renderContent(entry)}
       </div>
     );
@@ -16,10 +17,74 @@ class NetworkDetails extends Component {
   _renderContent = (entry) => {
     if (entry) {
       const { method, request, response, error } = entry;
-      return <ReactJson name="grpc" enableClipboard={false} src={ { method, request, response, error } } /> 
+      return <ReactJson name="grpc" enableClipboard={false} src={ { method, request, response, error, largeList } } /> 
     }
   }
 }
 
-const mapStateToProps = state => ({ entry: state.network.selectedEntry })
-export default connect(mapStateToProps)(NetworkDetails)
+const mapStateToProps = state => ({ entry: state.network.selectedEntry });
+export default connect(mapStateToProps)(NetworkDetails);
+
+var largeList = [
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+  "123456789",
+]
+
