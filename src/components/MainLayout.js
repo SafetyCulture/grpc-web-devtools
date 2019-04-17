f7e2793803373b8fef122b7dbd820f5d81a380ee
Roger Chapman <rogchap@gmail.com>
2019-04-17 21:44:16 +1000

    Implement a split pane for the network log

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2019 SafetyCulture Pty Ltd. All Rights Reserved.
 
 import React, { Component } from 'react';
+import Split from 'react-split'
 import './MainLayout.css';
 import NetworkDetails from './NetworkDetails';
 import NetworkList from './NetworkList';
@@ -9,12 +10,15 @@ class MainLayout extends Component {
     return (
       <div className="vbox flex-auto">
         <div className="shadow-split-widget hbox widget">
-          <div className="shadow-split-widget-contents shadow-split-widget-sidebar vbox">
+          <Split
+            className="hbox flex-auto"
+            sizes={[30, 70]}
+            gutterSize={5}
+            cursor="ew-resize"
+          >
             <NetworkList />
-          </div>
-          <div className="shadow-split-widget-contents shadow-split-widget-main">
             <NetworkDetails />
-          </div>
+          </Split>
         </div>
       </div>
     );
@@ -22,3 +26,11 @@ class MainLayout extends Component {
 }
 
 export default MainLayout
+
+
+// <div className="shadow-split-widget-contents shadow-split-widget-sidebar vbox">
+//             <NetworkList />
+//           </div>
+//           <div className="shadow-split-widget-contents shadow-split-widget-main">
+//             <NetworkDetails />
+//           </div>
