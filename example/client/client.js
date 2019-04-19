e7ed362431f961be6b37c7dc58f57732026e1fd7
Roger Chapman <rogchap@gmail.com>
2019-04-19 11:53:38 +1000

    Change example 2 to use callback client

--- example/client/client.js
+++ example/client/client.js
@@ -2,38 +2,43 @@
 
 import Sentencer from 'sentencer';
 import { ExampleTwoRequest } from './example2_pb';
-import { ExampleServicePromiseClient } from './example_grpc_web_pb';
+import { ExampleServicePromiseClient, ExampleServiceClient } from './example_grpc_web_pb';
 import { ExampleOneRequest } from './example_pb';
 
 const __DEV__ = true;
 const enableDevTools = window.__GRPCWEB_DEVTOOLS__ || (() => { });
 
 const body = document.getElementsByTagName('body')
-const client = new ExampleServicePromiseClient('http://0.0.0.0:18080', null, null);
+const client = new ExampleServicePromiseClient('http://0.0.0.0:18080');
+const client2 = new ExampleServiceClient('http://0.0.0.0:18080');
 
 if (__DEV__) {
   enableDevTools([
     client,
+    client2,
   ])
 }
 
 function exampleOne() {
-  const req1 = new ExampleOneRequest();
-  req1.setMsg(Sentencer.make("This is {{ an_adjective }} {{ noun }}."));
-  client.exampleOne(req1).then(res => {
+  const req = new ExampleOneRequest();
+  req.setMsg(Sentencer.make("This is {{ an_adjective }} {{ noun }}."));
+  client.exampleOne(req).then(res => {
     document.body.innerHTML += `<div>${res.getMsg()}</div>`
   }).catch(console.error)
 }
 
 function exampleTwo() {
-  const req1 = new ExampleTwoRequest();
-  client.exampleTwo(req1).then(res => {
+  const req = new ExampleTwoRequest();
+  client2.exampleTwo(req, null, (err, res) => {
+    if (err) {
+      return console.error(err);
+    }
     document.body.innerHTML += `<div>${res.getMsg()}</div>`
-  }).catch(console.error)
+  })
 }
 
 function alwaysError() {
-  client.alwaysError(new ExampleOneRequest()).catch(()=>{})
+  client.alwaysError(new ExampleOneRequest()).catch(() => { })
 }
 
 exampleOne()
