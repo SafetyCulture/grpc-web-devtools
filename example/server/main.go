6da1fce20d96d9cdeb31cad48eaa701f0945a0ac
Roger Chapman <rogchap@gmail.com>
2019-05-11 16:00:35 +1000

    Add a streaming example to the server

--- example/server/main.go
+++ example/server/main.go
@@ -41,6 +41,24 @@ func (s *server) AlwaysError(context.Context, *ExampleOneRequest) (*ExampleOneRe
 	return nil, status.Error(code, "an error ocurred")
 }
 
+func (s *server) StreamingExample(req *StreamRequest, stream ExampleService_StreamingExampleServer) error {
+  if req.GetError() {
+    time.Sleep(time.Second * 10)
+    code := codes.Code(rand.Int31n(15) + 1)
+	  return status.Error(code, "a server streaming error ocurred")
+  }
+
+  for start := time.Now(); time.Since(start) < time.Minute; {
+    t := ServerTime{
+      Time: time.Now().String(),
+    }
+    _ = stream.Send(&t)
+    time.Sleep(time.Second * 10)
+  }
+
+  return nil
+}
+
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
